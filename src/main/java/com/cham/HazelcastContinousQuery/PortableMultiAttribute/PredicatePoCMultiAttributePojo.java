228b88ae04a0ece5b9664b84d936718b15fa46d9
cwijayasundara <cwijayasundara@gmail.com>
2018-04-24 10:42:52 +0100

    added the continous query cache support

--- src/main/java/com/cham/HazelcastContinousQuery/PortableMultiAttribute/PredicatePoCMultiAttributePojo.java
+++ src/main/java/com/cham/HazelcastContinousQuery/PortableMultiAttribute/PredicatePoCMultiAttributePojo.java
@@ -2,9 +2,11 @@ package com.cham.HazelcastContinousQuery.PortableMultiAttribute;
 
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.config.QueryCacheConfig;
 import com.hazelcast.config.SerializationConfig;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
+import com.hazelcast.map.QueryCache;
 import com.hazelcast.query.SqlPredicate;
 
 import java.util.Collection;
@@ -12,26 +14,51 @@ import java.util.Iterator;
 
 public class PredicatePoCMultiAttributePojo {
 
+    private final static String cacheName="employee-map-cache";
+    private final static String sqlPredicate="name like A% AND age>15 AND salary<600";
+    private final static String mapName="employee-map";
+    private final static String portableClassName="com.cham.HazelcastContinousQuery.PortableMultiAttribute.PortableFactoryImpl";
+    private final static int factoryId=1;
+
     public static void main(String args[]){
 
+        // sql predicates
         ClientConfig clientConfig = new ClientConfig();
         SerializationConfig srzConfig = clientConfig.getSerializationConfig();
-        srzConfig.addPortableFactoryClass(1, "com.cham.HazelcastContinousQuery.PortableMultiAttribute.PortableFactoryImpl");
+        srzConfig.addPortableFactoryClass(factoryId, portableClassName);
+
+        // create the query cache
+        QueryCacheConfig queryCacheConfig = new QueryCacheConfig(cacheName);
+        clientConfig.addQueryCacheConfig(mapName, queryCacheConfig);
+
         HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
 
-        IMap<String, Employee> empMap = client.getMap("employee-map");
+        // create the IMap
+        IMap<String, Employee> serverEmpMap = client.getMap(mapName);
+        // populate data to the server Map
+        serverEmpMap.set("Alice", new Employee("Alice", 20, 500.50 ));
+        serverEmpMap.set("Bob", new Employee("Bob", 45, 1000.56 ));
+        serverEmpMap.set("Sam", new Employee("Sam", 35, 4567.89));
+        serverEmpMap.set("Tom", new Employee("Tom", 60, 5000.45));
+        serverEmpMap.set("Antoney", new Employee("Antoney",80, 345.56 ));
 
-        empMap.set("Alice", new Employee("Alice", 20, 500.50 ));
-        empMap.set("Bob", new Employee("Bob", 45, 1000.56 ));
-        empMap.set("Sam", new Employee("Sam", 35, 4567.89));
-        empMap.set("Tom", new Employee("Tom", 60, 5000.45));
-        empMap.set("Antoney", new Employee("Antoney",80, 345.56 ));
+        IMap<String, Employee> clientEmpMap = (IMap) client.getMap(mapName);
 
-        Collection<Employee> employees = empMap.values(new SqlPredicate( "name like A% AND age<100 AND salary>500"));
+        QueryCache<String, Employee> queryCache = clientEmpMap.getQueryCache(cacheName, new SqlPredicate(sqlPredicate),true);
+        Collection<Employee> employeeCollection = queryCache.values();
 
-        Iterator itr = employees.iterator();
-        while (itr.hasNext()){
-            System.out.println(itr.next());
+        while (true) {
+            try {
+                Thread.sleep(1000);
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            int size = queryCache.size();
+            System.out.println("Continuous query cache size = " + size);
+            Iterator itr = employeeCollection.iterator();
+            while(itr.hasNext()){
+                System.out.println(itr.next());
+            }
         }
     }
 }
